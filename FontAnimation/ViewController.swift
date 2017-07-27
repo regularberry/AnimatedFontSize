b0dc0cad02faa53adbdba3db82788ee107542dbd
Sean Berry <seanb@livefront.com>
2017-07-27 18:06:08 -0500

    Optional cross-fading

--- FontAnimation/ViewController.swift
+++ FontAnimation/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     var isSmall: Bool = true
+    var crossFading: Bool {
+        return crossFadeSwitch.isOn
+    }
+    
+    @IBOutlet weak var crossFadeSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,14 +30,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
     @IBAction func reset(_ sender: Any) {
-        let font = label.font.withSize(100)
+        let font = label.font.withSize(20)
         var bounds = label.bounds
         label.font = font
         bounds.size = label.intrinsicContentSize
         label.bounds = bounds
-        isSmall = false
+        isSmall = true
     }
 
     @IBAction func animateFont(_ sender: Any) {
@@ -45,7 +49,31 @@ class ViewController: UIViewController {
     }
     
     func enlarge() {
+        if crossFading {
+            enlargeWithCrossFade()
+            return
+        }
         
+        var bounds = label.bounds
+        
+        label.font = label.font.withSize(100)
+        bounds.size = label.intrinsicContentSize
+        
+        let scaleX = label.frame.size.width / bounds.size.width
+        let scaleY = label.frame.size.height / bounds.size.height
+        
+        label.bounds = bounds
+        label.transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
+        
+        let duration = 1.0
+        
+        UIView.animate(withDuration: duration, animations: {
+            self.label.transform = .identity
+        }, completion: { done in
+        })
+    }
+    
+    func enlargeWithCrossFade() {
         let labelCopy = label.copyLabel()
         view.addSubview(labelCopy)
         
@@ -78,6 +106,11 @@ class ViewController: UIViewController {
     }
     
     func shrink() {
+        if crossFading {
+            shrinkWithCrossFade()
+            return
+        }
+        
         let labelCopy = label.copyLabel()
         labelCopy.font = label.font.withSize(20)
         
@@ -93,6 +126,38 @@ class ViewController: UIViewController {
             self.label.font = labelCopy.font
             self.label.transform = .identity
             self.label.bounds = bounds
+        })
+    }
+    
+    func shrinkWithCrossFade() {
+        let labelCopy = label.copyLabel()
+        view.addSubview(labelCopy)
+        
+        label.font = label.font.withSize(20)
+        
+        var bounds = label.bounds
+        bounds.size = label.intrinsicContentSize
+        let scaleX = bounds.size.width / label.frame.size.width
+        let scaleY = bounds.size.height / label.frame.size.height
+        
+        label.transform = CGAffineTransform(scaleX: 1 / scaleX, y: 1 / scaleY)
+        label.alpha = 0.0
+        
+        let duration = 1.0
+        UIView.animate(withDuration: duration, animations: {
+            labelCopy.transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
+            self.label.transform = .identity
+        }, completion: { done in
+            self.label.transform = .identity
+            self.label.bounds = bounds
+        })
+        
+        let multiple: Double = 5
+        UIView.animate(withDuration: duration/multiple, delay: duration - (duration / multiple), options: .curveLinear, animations: {
+            labelCopy.alpha = 0
+            self.label.alpha = 1
+        }, completion: { done in
+            labelCopy.removeFromSuperview()
         })
     }
 }
